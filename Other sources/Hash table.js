9aba6a1ad44bc5e37e56a41d1bdf46d21592eefb
Sandip <sandiproy10@yahoo.com>
2019-07-09 22:32:59 +0800

    Create Hash table.js

---
+++ Other sources/Hash table.js
@@ -0,0 +1,47 @@
+// task : implement your own hash table 
+
+class HashTable{
+	constructor(size){
+		this.data = new Array(size)
+	}
+ 
+  _hash(key) {
+  //random hash function to generate a hash for a given key
+	let hash = 0;
+	for (let i=0; i<key.length; i++){
+		hash = (hash+ key.charCodeAt(i)*i)
+		%this.data.length
+	}
+	return hash;
+  }
+  
+  set(key,value){
+  //setting the key velue pair  of the hash table
+	let address = this._hash(key);
+	if (!this.data[address]){
+		this.data[address]=[] // if there is no collision then create a new empty array to store the key value pair
+	}
+	this.data[address].push([key,value])
+	console.log(this.data)
+  }
+
+  //given any key retrieving the value stored with that key
+  get(key){
+    let address = this._hash(key)
+    const holdingBucket= this.data[address]
+    if (holdingBucket){
+      for (let i=0; i<holdingBucket.length ; i++){
+        if (holdingBucket[i][0] === key){
+          return holdingBucket[i][1]
+        }
+      }
+    }
+    return undefined
+    }
+}
+
+const myHashTable = new HashTable(50); // initialzing with array size of 50
+myHashTable.set("grapes",1000);
+myHashTable.set("oranges",500);
+myHashTable.set("watermelons ",75);
+myHashTable.get("grapes");
