b929f653a2e3a4d27f1b31161347c3f9aa387196
Sandip <sandiproy10@yahoo.com>
2019-07-15 22:21:42 +0800

    Create Problem 1108: Defanging an IP Address.js

---
+++ String problems/Problem 1108: Defanging an IP Address.js
@@ -0,0 +1,19 @@
+//leetcode problem : 1108. Defanging an IP Address
+/*Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".*/
+
+
+var defangIPaddr = function(address) {
+  let array = address.split('.') 
+  return array.join('[.]')
+};
+defangIPaddr("255.100.50.0")
+
+
+//another method 
+var defangIPaddr = function(address) {
+    return address.replace(/[.]/g, '[.]');
+};
+
+defangIPaddr("255.100.50.0")
