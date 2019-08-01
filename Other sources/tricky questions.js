904be1fb08813c7eff5411120169f0ad72f655f5
Sandip <sandiproy10@yahoo.com>
2019-08-01 22:27:19 +0800

    Update tricky questions.js

--- Other sources/tricky questions.js
+++ Other sources/tricky questions.js
@@ -27,3 +27,45 @@ dynamically scoped to the window object */
 
 d.say()()  /* outputs the d object. here this refers to the d object since we are using the arrow function which 
 is lexically scoped, instead of functionally scoped. */
+
+
+//problem: what is the output of the following: 
+
+let arr = [1,2,3];
+arr.map(item => {return item*2});
+console.log(arr)  
+//output will be [1,2,3]
+
+
+
+//what is the ouput of the following code
+
+var arr1 = [{name:"sandip",age:16},{name:'joy',age:17}];
+
+var arr2 = [1,2,3];
+arr1.forEach(item => {item.age = item.age +1});
+console.log(arr1)
+
+arr2.forEach(item => { return  item*2})
+
+console.log(arr2)
+
+
+
+//problem:  write the output 
+
+let a ={
+  url :'996'
+};
+let b = {
+  name:"sandip",
+  url: a
+};
+let c = Object.assign({},b);
+a.url = a.url + '.icu'
+console.log(c)
+
+//output is :
+//{name: 'sandip', url : {
+//   url: '996.icu'
+// }}
