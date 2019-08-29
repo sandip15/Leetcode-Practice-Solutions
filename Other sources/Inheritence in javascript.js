3e7ccdfa9ef8c732c551c38b3e40017b4157d99e
Sandip <sandiproy10@yahoo.com>
2019-08-29 15:00:45 +0800

    Update Inheritence in javascript.js
    added new codes

--- Other sources/Inheritence in javascript.js
+++ Other sources/Inheritence in javascript.js
@@ -1,9 +1,28 @@
 // How the inheritence works in javascript
 // Prototype : Prototype is a property that every function you create in javasctipt has, that points to an object.
-/* how to inherit properties from parent function
-  to inherit properties of the parent function we need to use the call function 
-  to inherit the methods of the parent function we need to use the object.create to link prototypes of parent and child.
-*/
+
+//Basics : 
+
+function Animal(name,energy){
+  let animal = Object.create(Animal.prototype)
+  animal.name = name;
+  animal.energy = energy;
+  return animal;
+}
+
+/*when we use the above method with creating another object and then returning that object, we instantiate 
+the new object by the following way: */
+const leo = Animal ('leo',10);
+const snoop = Animal('snoop',12);
+
+function Animal (name,energy){
+  this.name = name;
+  this.energy = energy;
+}
+// when we use the keyword 'new' during instantiating the new object then we follow the above method.
+const leo = new Animal ("leo",10);
+const snoop = new Animal ('snoop',12);
+
 
 function Animal(name,type){
   this.name = name;
@@ -17,10 +36,10 @@ Animal.prototype.sing = function (){
 }
 
 function Dog(name,type){
-  Animal.call(this,name,type);
+  Animal.call(this,name,type); //to inherit properties of the parent function we need to use the call function 
 }
 
-Dog.prototype = Object.create(Animal.prototype)
+Dog.prototype = Object.create(Animal.prototype) //to inherit the methods of the parent function we need to use the object.create to link prototype
 let pet = new Dog("max",'mixed');
 pet.shout()
 pet.sing()
