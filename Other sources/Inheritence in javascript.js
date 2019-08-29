2eac0ba6baf65ee01fa2575511c74ee67f6beef4
Sandip <sandiproy10@yahoo.com>
2019-08-29 14:50:59 +0800

    Update Inheritence in javascript.js

--- Other sources/Inheritence in javascript.js
+++ Other sources/Inheritence in javascript.js
@@ -1,5 +1,5 @@
 // How the inheritence works in javascript
-
+// Prototype : Prototype is a property that every function you create in javasctipt has, that points to an object.
 /* how to inherit properties from parent function
   to inherit properties of the parent function we need to use the call function 
   to inherit the methods of the parent function we need to use the object.create to link prototypes of parent and child.
