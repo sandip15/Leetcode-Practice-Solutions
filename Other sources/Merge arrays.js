f4ec8eb33d86f36d9b035951e66c1f9fa5c6b1dd
Sandip <sandiproy10@yahoo.com>
2020-05-02 22:44:20 +0600

    Update Merge arrays.js

--- Other sources/Merge arrays.js
+++ Other sources/Merge arrays.js
@@ -1,4 +1,4 @@
-//problem:given two sorted arrays merge them together into one array so that the resultant array also maintains this sorting order even after the merging
+//Given two sorted arrays merge them together into one array so that the resultant array also maintains this sorting order even after the merging
 
 var addElement = function (arr1,arr2){
   const mergedArray =[];
