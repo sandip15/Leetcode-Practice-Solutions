d51c76fe47a6e0589de9c744fbcbf9d4293043a8
Sandip <sandiproy10@yahoo.com>
2020-05-16 22:41:43 +0600

    Update guess output.js

--- Other sources/guess output.js
+++ Other sources/guess output.js
@@ -16,7 +16,7 @@ console.log('4');
 //answer:
 // output is 2 ,4 ,3 ,1 
 
-//2. problem: what is the output of the following code:
+//2. problem: what is the output of the following piece of code:
 
 var bb = 1;
 function aa(bb){
