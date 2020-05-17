717ab1a268d6cba0981fbe333b52b3962877ee15
Sandip <sandiproy10@yahoo.com>
2020-05-17 23:54:25 +0600

    Update Inheritence in javascript.js

--- Other sources/Inheritence in javascript.js
+++ Other sources/Inheritence in javascript.js
@@ -1,4 +1,4 @@
-// basic concept: important question to be asked during an interview : How the inheritence works in javascript ：
+// basic concept: How the inheritence works in javascript
 // Prototype : Prototype is a property that every function you create in javasctipt has, that points to an object.
 
 //Basics :  implement one sample example for understanding how inheritence works in javascript
