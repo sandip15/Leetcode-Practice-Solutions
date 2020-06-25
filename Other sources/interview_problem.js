19021d4bf02678c8640470a99aec9434c5f45f07
Sandip <sandiproy10@yahoo.com>
2020-06-25 23:43:14 +0600

    Create interview_problem.js

---
+++ Other sources/interview_problem.js
@@ -0,0 +1,30 @@
+//Write a sum method which will work properly when invoked using either syntax below : sum(x)(y) and sum (x,y)
+
+//first method
+
+// function sum(x){
+//   if (arguments.length == 2){
+//     return arguments[0]+ arguments[1]
+//   }else{
+//     return function (y){
+//       return x+y
+//     }
+//   }
+// }
+
+// sum (1)(3)
+
+
+//second method
+
+function sum (x,y){
+  if (y !== undefined){
+    return (x+y)
+  }else{
+    return function (y){
+      return x+y
+    }
+  }
+}
+
+sum(4)(5)
