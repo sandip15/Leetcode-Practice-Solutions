726def81e8c187c6d0e47a92b67046bb8dca810c
Sandip <sandiproy10@yahoo.com>
2020-06-27 17:11:38 +0600

    Create slice_splice_split.js

---
+++ important concept/slice_splice_split.js
@@ -0,0 +1,30 @@
+/* Note about different array/string methods in js */
+
+Slice ( )
+/*
+Copies elements from an array
+Returns them as a new array
+Doesn’t change the original array
+Starts slicing from … until given index: array.slice (from, until)
+Slice doesn’t include “until” index parameter
+Can be used both for arrays and strings
+*/
+
+Splice ( )
+/*
+Used for adding/removing elements from array
+Returns an array of removed elements
+Changes the array
+For adding elements: array.splice (index, number of elements, element)
+For removing elements: array.splice (index, number of elements)
+Can only be used for arrays
+*/
+
+Split ( )
+/*
+Divides a string into substrings
+Returns them in an array
+Takes 2 parameters, both are optional: string.split(separator, limit)
+Doesn’t change the original string
+Can only be used for strings
+*/
