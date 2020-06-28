fe0180bf06fd2f17deefb4031df7d77df27ce6e3
Sandip <sandiproy10@yahoo.com>
2020-06-28 22:50:10 +0600

    Update interview_problem.js

--- Other sources/interview_problem.js
+++ Other sources/interview_problem.js
@@ -2,17 +2,16 @@
 
 //first method
 
-// function sum(x){
-//   if (arguments.length == 2){
-//     return arguments[0]+ arguments[1]
-//   }else{
-//     return function (y){
-//       return x+y
-//     }
-//   }
-// }
-
-// sum (1)(3)
+function sum(x){
+  if (arguments.length == 2){
+    return arguments[0]+ arguments[1]
+  }else{
+    return function (y){
+      return x+y
+    }
+  }
+}
+sum (1)(3)
 
 
 //second method
