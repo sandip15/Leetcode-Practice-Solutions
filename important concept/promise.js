aab3365c095ddd3e069ecd517221d614b6cbab99
Sandip <sandiproy10@yahoo.com>
2020-07-06 17:32:27 +0600

    Create promise.js

---
+++ important concept/promise.js
@@ -0,0 +1,33 @@
+//practicing promise to handle asynch operations in js
+
+const add = function (a,b){
+  return new Promise((resolve,reject) => {
+    let sum = a+b ;
+    if (sum){
+      resolve(sum)
+    }else{
+      reject("sorry we cant add them!!")
+    }
+  })
+}
+
+const sub = function (a,b){
+  return new Promise (function (resolve,reject){
+    let subt = a-b;
+    if (subt){
+      resolve(subt)
+    }else{
+      reject ("sorry we cant subtract them!!")
+    }
+  })
+}
+
+add(2,2).then(function (added){.    //chaining all the promises together
+  return sub(added,3)
+}).then((subtracted) => {
+  return add(subtracted,5)
+}).then (function (added){
+  return sub(added, 3)
+}).catch((err) => {
+  console.log(err)
+})
