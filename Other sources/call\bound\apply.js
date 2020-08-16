3b4145127b68acdb74c24ee9567fd369953d2434
Sandip Roy <sandiproy10@yahoo.com>
2020-08-16 23:28:06 +0600

    Update call\bound\apply.js

--- Other sources/call\bound\apply.js
+++ Other sources/call\bound\apply.js
@@ -2,7 +2,7 @@
 // question: implement the use of call and apply method to change the value of this 
 // generally call is used for passing the inheritence from the mother class to the children class
 
-//question: practicing the use of (javasctipt built method) call and apply method to change the value of this 
+//question: practicing the use of (javasctipt built in  method) call and apply method to change the value of this 
 // the use of call method: generally call is used for passing the inheritence from the mother class to its children class
 
 
@@ -15,7 +15,7 @@ let obj = {c:3,d:5}
 /* The first parameter is the object to use as 'this', subsequent parameters are passed as  arguments in the function call */
 add.call(obj,1,5)
 
-/* The first parameter is the object to use as 'this', the second is an array whose members are used as the arguments in the function call */
+/* The first parameter is the object to be used as 'this', the second one is an array whose members are used as the arguments in the function call */
 add.apply(obj,[1,2])
 
 ----------------
