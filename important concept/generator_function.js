2f4f9393eb29d71f1219a1363456283e290a803e
Sandip Roy <sandiproy10@yahoo.com>
2020-09-06 23:19:52 +0600

    Update generator_function.js

--- important concept/generator_function.js
+++ important concept/generator_function.js
@@ -1,5 +1,5 @@
 // Practicing use of generator functions
-// Using generator function will pause the execution of the function and resumes it until yield
+// Using generator function will pause the execution of the function and resumes it until yield, very useful for pausing the execution of some context
 
 const myGen = function* (){
   const one = yield 1;  // first resume of the funciton execution
