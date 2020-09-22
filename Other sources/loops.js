038d9886909a148038eb0ca4d22d2c657ce92c7f
Sandip Roy <sandiproy10@yahoo.com>
2020-09-22 23:39:40 +0600

    Update loops.js

--- Other sources/loops.js
+++ Other sources/loops.js
@@ -2,7 +2,7 @@
 
 // Question: leetcode problem number 771(easy)
 /* we're given with two strings, J representing the types of stones that are jewels, and S representing the stones you have. 
-Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+Each character in S is a type of stone you have.  we need to find out how many of the stones that we are having are also jewels.
 
 The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, 
 so "a" is considered a different type of stone from "A". */
