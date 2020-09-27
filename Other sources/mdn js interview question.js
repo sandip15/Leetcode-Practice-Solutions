d128dd896781e0f193efecb13cb44b290680be48
Sandip Roy <sandiproy10@yahoo.com>
2020-09-27 23:25:16 +0600

    Update mdn js interview question.js

--- Other sources/mdn js interview question.js
+++ Other sources/mdn js interview question.js
@@ -1,4 +1,4 @@
 //20 essential javascript interview questions:
 
-//still need to update, for more updates visit the following link
+//have not updated yet, for more updates visit the following link
 //https://adevait.com/javascript-developers/interview-questions
