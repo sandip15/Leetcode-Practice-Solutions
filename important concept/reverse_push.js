59334fa313f170a1a0c0f3b9dee78eaa160d2498
Sandip Roy <sandiproy10@yahoo.com>
2020-10-02 23:39:35 +0600

    Update reverse_push.js

--- important concept/reverse_push.js
+++ important concept/reverse_push.js
@@ -5,7 +5,7 @@ reference : https://www.toptal.com/javascript/interview-questions
 var arr1 = "john".split('');
 var arr2 = arr1.reverse();
 var arr3 = "jones".split('');
-arr2.push(arr3);    // calling push on some array actually modifies the original array also (read the difference between push and concat)
+arr2.push(arr3);    // calling array.push function on some array actually modifies the original array also (read the difference between push and concat)
 
 console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(-1));
 console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(-1));
